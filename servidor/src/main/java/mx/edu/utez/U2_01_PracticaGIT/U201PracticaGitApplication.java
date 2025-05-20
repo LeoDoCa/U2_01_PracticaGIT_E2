e9aa4380aa05f0d0b4bede471be3d92f07ce8eec
Anett Yomali Vera Carbajal <132587180+AnettVera@users.noreply.github.com>
2025-05-20 15:54:58 -0600

    primer commit

---
+++ servidor/src/main/java/mx/edu/utez/U2_01_PracticaGIT/U201PracticaGitApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.U2_01_PracticaGIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U201PracticaGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U201PracticaGitApplication.class, args);
+	}
+
+}
---
+++ servidor/src/test/java/mx/edu/utez/U2_01_PracticaGIT/U201PracticaGitApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.U2_01_PracticaGIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U201PracticaGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
